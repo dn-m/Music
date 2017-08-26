cc4253bfeb93e588f95ff2e65badb276b227f713
James <jbean46@gmail.com>
2017-08-26 15:23:04 -0400

    Tidy up

--- Sources/Pitch/Set+NoteNumberRepresentable.swift
+++ Sources/Pitch/Set+NoteNumberRepresentable.swift
@@ -39,10 +39,10 @@ extension Collection where Element == Pitch.Class {
     // FIXME: Lift `rotated` to Collection rather than Array
     private var rotations: [[Pitch.Class]] {
         let values = Array(self)
-        return (0..<values.count).map { amount in values.rotated(by: amount) }
+        return (0..<values.count).map { values.rotated(by: $0) }
     }
 
-    // Transpose the set such that the first value is `0`.
+    /// Transpose the set such that the first value is `0`.
     public var reduced: [Pitch.Class] {
         assert(count > 0)
         return map { $0 - first! }
@@ -50,6 +50,14 @@ extension Collection where Element == Pitch.Class {
 
     public var inversion: [Pitch.Class] {
         return map { $0.inversion }
+    }
+}
+
+extension BidirectionalCollection where Element == Pitch.Class {
+
+    // Invariant: self is sorted, is not empty
+    var span: Pitch.Class {
+        return last! - first!
     }
 }
 
@@ -62,12 +70,4 @@ func mostLeftPacked(_ values: [[Pitch.Class]]) -> [Pitch.Class] {
     assert(!values.isEmpty)
     guard values.count > 1 else { return values.first! }
     return values.sorted { $0.intervals.lexicographicallyPrecedes($1.intervals) }.first!
-}
-
-extension BidirectionalCollection where Element == Pitch.Class {
-
-    // Invariant: self is sorted, is not empty
-    var span: Pitch.Class {
-        return last! - first!
-    }
 }
