8d2f6bb84287f0159912439f07e555c1c2a972e1
James <jbean46@gmail.com>
2017-08-26 17:39:04 -0400

    Move BeatContext

---
+++ Sources/MusicModel/BeatContext.swift
@@ -0,0 +1,45 @@
+//
+//  BeatContext.swift
+//  MusicModel
+//
+//  Created by James Bean on 8/26/17.
+//
+
+import Math
+import Rhythm
+import Tempo
+import Meter
+
+// TODO: Move to `dn-m/MetronomeController`
+/// Information about a given beat within a `Meter`.
+public struct BeatContext {
+
+    // MARK: - Instance Properties
+
+    /// - returns: Metrical offset from start of a `Meter.Structure`.
+    public var metricalOffset: MetricalDuration {
+        let rangeOffsetFraction = meterContext.meter.range.lowerBound
+        let rangeOffset = rangeOffsetFraction.numerator /> rangeOffsetFraction.denominator
+        return meterContext.offset + offset - rangeOffset
+    }
+
+    /// Meter containing `BeatContext`.
+    public let meterContext: Meter.Context
+
+    /// Metrical offset of beat within `Meter`.
+    public let offset: MetricalDuration
+
+    // MARK: - Initializers
+
+    /// Creates a `BeatContext` with the given `subdivision` and `position`.
+    public init(
+        meterContext: Meter.Context,
+        offset: MetricalDuration,
+        interpolation: Tempo.Interpolation
+    )
+    {
+        self.meterContext = meterContext
+        self.offset = offset
+    }
+}
+
