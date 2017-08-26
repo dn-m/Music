f413b554952c71d7243896b756a4faff720539b8
James <jbean46@gmail.com>
2017-08-26 15:15:17 -0400

    Remove fp-ness for now

--- Sources/Pitch/Set+NoteNumberRepresentable.swift
+++ Sources/Pitch/Set+NoteNumberRepresentable.swift
@@ -25,15 +25,21 @@ extension Collection where Element: NoteNumberRepresentable {
 
 extension Collection where Element == Pitch.Class {
 
-    /// Normal form of a Pitch.Class segment
-    public var normalForm: [Pitch.Class] {
-        return sorted().rotations |> mostCompact >>> mostLeftPacked
-    }
-
     /// - Returns: The Prime Form
     public var primeForm: [Pitch.Class] {
         guard !isEmpty else { return map { $0 } }
         return mostLeftPacked([normalForm, inversion.normalForm]).reduced
+    }
+
+    /// Normal form of a Pitch.Class segment
+    public var normalForm: [Pitch.Class] {
+        return mostLeftPacked(mostCompact(sorted().rotations))
+    }
+
+    // FIXME: Lift `rotated` to Collection rather than Array
+    private var rotations: [[Pitch.Class]] {
+        let values = Array(self)
+        return (0..<values.count).map { amount in values.rotated(by: amount) }
     }
 
     // Transpose the set such that the first value is `0`.
@@ -44,12 +50,6 @@ extension Collection where Element == Pitch.Class {
 
     public var inversion: [Pitch.Class] {
         return map { $0.inversion }
-    }
-
-    // FIXME: Lift rotated to Collection rather than Array
-    private var rotations: [[Pitch.Class]] {
-        let values = Array(self)
-        return (0..<values.count).map { amount in values.rotated(by: amount) }
     }
 }
 
@@ -71,58 +71,3 @@ extension BidirectionalCollection where Element == Pitch.Class {
         return last! - first!
     }
 }
-
-// FIXME: Move to lower level module
-
-precedencegroup CompositionPrecedence {
-    associativity: right
-    higherThan: BitwiseShiftPrecedence
-}
-
-precedencegroup RightApplyPrecedence {
-    associativity: right
-    higherThan: AssignmentPrecedence
-    lowerThan: TernaryPrecedence
-}
-
-precedencegroup LeftApplyPrecedence {
-    associativity: left
-    higherThan: AssignmentPrecedence
-    lowerThan: TernaryPrecedence
-}
-
-/// Compose | Applies one function to the result of another function to produce a third function.
-infix operator <<< : CompositionPrecedence
-infix operator >>> : CompositionPrecedence
-
-/// Pipe Backward | Applies the function to its left to an argument on its right.
-infix operator <| : RightApplyPrecedence
-
-/// Pipe forward | Applies an argument on the left to a function on the right.
-infix operator |> : LeftApplyPrecedence
-
-/// Compose | Applies one function to the result of another function to produce a third function.
-///
-///     f : B -> C
-///     g : A -> B
-///     (f • g)(x) === f(g(x)) : A -> B -> C
-public func <<< <A, B, C> (f: @escaping (B) -> C, g : @escaping (A) -> B) -> (A) -> C {
-    return { x in
-        return f(g(x))
-    }
-}
-
-public func >>> <A, B, C> (f: @escaping (A) -> B, g : @escaping (B) -> C) -> (A) -> C {
-    return { x in
-        return g(f(x))
-    }
-}
-
-func <| <A,B> (_ f: @escaping (A) -> B, _ x: A) -> B {
-    return f(x)
-}
-
-func |> <A,B>(_ x: A, _ f: @escaping (A) -> B) -> B {
-    return f(x)
-}
-
