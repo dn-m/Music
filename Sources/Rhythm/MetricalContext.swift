ad03cde1c63aa4bc8f523f9c59e6f0328dfcb338
James <jbean46@gmail.com>
2018-06-18 16:37:45 -0700

    Create funcs to make it slightly cleaner to create rhythmic values

--- Sources/Rhythm/MetricalContext.swift
+++ Sources/Rhythm/MetricalContext.swift
@@ -9,3 +9,15 @@
 /// The metrical context of a given `Leaf` (i.e., whether or not the musical event is "tied"
 /// from the previous event, and whether or not is a "rest" or an actual event.
 public typealias MetricalContext <Element> = ContinuationOrInstance<AbsenceOrEvent<Element>>
+
+public func tie <T> () -> MetricalContext<T> {
+    return .continuation
+}
+
+public func event <T> (_ value: T) -> MetricalContext<T> {
+    return .instance(.event(value))
+}
+
+public func rest <T> () -> MetricalContext<T> {
+    return .instance(.absence)
+}
---
+++ Tests/RhythmTests/RhythmTests.swift
@@ -0,0 +1,29 @@
+//
+//  RhythmTests.swift
+//  RhythmTests
+//
+//  Created by James Bean on 6/18/18.
+//
+
+import XCTest
+import MetricalDuration
+import Rhythm
+
+class RhythmTests: XCTestCase {
+    
+    func testUsage() {
+        let tree = 4/>8 * [1,2,3,4,1,1]
+        let leaves: [MetricalContext<()>] = [
+            .instance(.event(())),
+            .instance(.event(())),
+            .instance(.event(())),
+            .instance(.event(())),
+            .instance(.event(())),
+            rest()
+        ]
+        let rhythm = tree * leaves
+    }
+}
+
+
+
