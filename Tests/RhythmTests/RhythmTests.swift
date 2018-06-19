2a75f19f3f46c263f97b1421202a2222c166ac4c
James <jbean46@gmail.com>
2018-06-18 17:55:46 -0700

    Drastically streamline tests

--- Tests/RhythmTests/RhythmTests.swift
+++ Tests/RhythmTests/RhythmTests.swift
@@ -46,56 +46,18 @@ class RhythmTests: XCTestCase {
     }
 
     func testLengthsTiesAndEvents() {
-
-        let durations = 4/>8 * [1,1,1,1]
-
-        let contexts: [MetricalContext<Int>] = [
-            .continuation,
-            .instance(.event(1)),
-            .continuation,
-            .instance(.event(1))
-        ]
-
-        let rhythmTree = Rhythm(durations, contexts)
-
-        XCTAssertEqual(lengths(of: [rhythmTree]), [1/>8, 2/>8, 1/>8])
+        let rhythm = Rhythm<()>(4/>8, [tie(), event(()), tie(), event(())])
+        XCTAssertEqual(lengths(of: [rhythm]), [1/>8, 2/>8, 1/>8])
     }
 
     func testLengthsTiesEventsAndRests() {
-
-        let durations = 4/>8 * [1,1,1,1]
-
-        let contexts: [MetricalContext<Int>] = [
-            .continuation,
-            .instance(.event(1)),
-            .continuation,
-            .instance(.absence)
-        ]
-
-        let rhythmTree = Rhythm(durations, contexts)
-
-        XCTAssertEqual(lengths(of: [rhythmTree]), [1/>8, 2/>8, 1/>8])
+        let rhythm = Rhythm(4/>8, [tie(), event(1), tie(), rest()])
+        XCTAssertEqual(lengths(of: [rhythm]), [1/>8, 2/>8, 1/>8])
     }
 
     func testLengths() {
-
-        let durations = 4/>8 * [1,2,1]
-
-        // tie, rest, event
-        let contexts: [MetricalContext<Int>] = [
-            .continuation,
-            .instance(.absence),
-            .instance(.event(1))
-        ]
-
-        let rhythmTree = Rhythm(durations, contexts)
-
-        // Create a sequence of rhythms:
-        //
-        //      tie, rest, event tie, rest, event tie, rest, event
-        //
-        let trees = (0..<3).map { _ in rhythmTree }
-
-        XCTAssertEqual(lengths(of: trees), [1/>8, 2/>8, 2/>8, 2/>8, 2/>8, 2/>8, 1/>8])
+        let rhythm = Rhythm(4/>8, [(1, tie()), (2, rest()), (1, event("uh"))])
+        let rhythms = (0..<3).map { _ in rhythm }
+        XCTAssertEqual(lengths(of: rhythms), [1/>8, 2/>8, 2/>8, 2/>8, 2/>8, 2/>8, 1/>8])
     }
 }
