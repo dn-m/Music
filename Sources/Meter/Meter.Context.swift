a9b6a244a1c78b19178392dfb94d11c2e0ff09bf
James <jbean46@gmail.com>
2018-08-06 22:50:27 -0700

    Add doc comments

--- Sources/Meter/Meter.Context.swift
+++ Sources/Meter/Meter.Context.swift
@@ -10,11 +10,20 @@ import Math
 
 extension Meter {
 
+    /// The context of a `Meter.Fragment` in a container.
     public struct Context: Equatable {
 
+        // MARK: - Instance Properties
+
+        /// The meter fragment.
         public let meter: Meter.Fragment
+
+        /// The offset of the meter fragment.
         public let offset: Fraction
 
+        // MARK: - Instance Properties
+
+        /// Create `Meter.Context` with the given `meter` at the given `offset`.
         public init(meter: Meter.Fragment, at offset: Fraction) {
             self.meter = meter
             self.offset = offset
