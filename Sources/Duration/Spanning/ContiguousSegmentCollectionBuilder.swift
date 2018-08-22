aaa1b166aa90ab44b3d3f4181f561e3af48b576c
James <jbean46@gmail.com>
2018-08-22 10:19:05 -0400

    Don't typealias Segment for builder

--- Sources/Duration/Spanning/ContiguousSegmentCollectionBuilder.swift
+++ Sources/Duration/Spanning/ContiguousSegmentCollectionBuilder.swift
@@ -20,23 +20,20 @@ public protocol ContiguousSegmentCollectionBuilder: class {
     /// Type of product which is built by `ContiguousSegmentCollectionBuilder`.
     associatedtype Product: ContiguousSegmentCollection
 
-    /// Segment-type contained by the `Product`.
-    typealias Spanner = Product.Segment
-
     // MARK: - Instance Properties
 
     /// Intermediate storage which is converted into the `Product`.
     //
     // FIXME: Consider using `OrderedDictionary` re: performance.
-    var intermediate: OrderedDictionary<Spanner.Metric,Spanner> { get set }
+    var intermediate: OrderedDictionary<Product.Segment.Metric,Product.Segment> { get set }
 
     /// Cumulative offset of spanners contained in `intermediate`.
-    var offset: Spanner.Metric { get set }
+    var offset: Product.Segment.Metric { get set }
 
     // MARK: - Instance Methods
 
     /// Adds the given `Segment` to the `intermediate`.
-    func add(_: Spanner) -> Self
+    func add(_: Product.Segment) -> Self
 
     /// Creates the final `Product`.
     func build() -> Product
@@ -47,7 +44,7 @@ extension ContiguousSegmentCollectionBuilder {
     /// Adds the given `element` to the `intermediate` with accumulating offsets.
     ///
     /// - Returns: `Self`.
-    @discardableResult public func add(_ element: Spanner) -> Self {
+    @discardableResult public func add(_ element: Product.Segment) -> Self {
         intermediate.append(element, key: offset)
         offset = offset + element.range.length
         return self
@@ -57,7 +54,7 @@ extension ContiguousSegmentCollectionBuilder {
     ///
     /// - Returns: `Self`.
     @discardableResult public func add <S: Sequence> (_ elements: S) -> Self
-        where S.Element == Spanner
+        where S.Element == Product.Segment
     {
         elements.forEach { _ = add($0) }
         return self
