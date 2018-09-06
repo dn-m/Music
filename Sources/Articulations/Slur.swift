7c192e3a96c2b9499b964c96479c6728c3094e0d
James <jbean46@gmail.com>
2018-09-06 09:32:39 -0400

    Add Slur enum

---
+++ Sources/Articulations/Slur.swift
@@ -0,0 +1,19 @@
+//
+//  Slur.swift
+//  Articulations
+//
+//  Created by James Bean on 9/6/18.
+//
+
+/// An articulative spanner which can be interpreted as an indicator of breath or bow control, or
+/// a phrase-level grouping.
+public enum Slur {
+
+    // MARK: - Cases
+
+    // Start a slur.
+    case start
+
+    // Stop a slur.
+    case stop
+}
