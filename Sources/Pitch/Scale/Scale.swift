befed09dd4ffaa8d4f15a0be2b3a73093751f913
James <jbean46@gmail.com>
2018-10-09 11:19:23 -0400

    Change structure of Scale, fix error message

--- Sources/Pitch/Scale/Scale.swift
+++ Sources/Pitch/Scale/Scale.swift
@@ -5,12 +5,13 @@
 //  Created by James Bean on 10/9/18.
 //
 
+import Algebra
+
 public struct Scale {
 
     // MARK: - Instance Properties
 
-    let first: Pitch
-    let intervals: IntervalPattern
+    let pitches: [Pitch]
 }
 
 extension Scale {
@@ -18,13 +19,19 @@ extension Scale {
     // MARK: - Initializers
 
     init(_ first: Pitch, _ intervals: IntervalPattern) {
-        self.first = first
-        self.intervals = intervals
+        // TODO: Make `Pitch` a monoid to allow `accumulatingRight`
+        var p = [first]
+        var accum: Pitch = 0
+        for i in intervals.intervals {
+            accum += i
+            p.append(first + accum)
+        }
+        self.pitches = p
     }
 
     init <S> (_ sequence: S) where S: Sequence, S.Element == Pitch {
         let sorted = sequence.sorted()
-        precondition(!sorted.isEmpty, "Cannot create a 'Chord' with an empty sequence of pitches")
+        precondition(!sorted.isEmpty, "Cannot create a 'Scale' with an empty sequence of pitches")
         self.init(sorted.first!, IntervalPattern(sorted.pairs.map { $1 - $0 }))
     }
 }
