9c28886c03b7770d107dc66213b79fa33901a887
James <jbean46@gmail.com>
2018-10-10 20:59:26 -0400

    Migrate CompoundIntervalDescriptor

---
+++ Sources/Pitch/IntervalDescriptor/CompoundIntervalDescriptor.swift
@@ -0,0 +1,40 @@
+//
+//  CompoundIntervalDescriptor.swift
+//  Pitch
+//
+//  Created by James Bean on 10/10/18.
+//
+
+/// A descriptor for intervals between two `Pitch` values which takes into account octave
+/// displacement.
+public struct CompoundIntervalDescriptor: IntervalDescriptor {
+
+    // MARK: - Instance Properties
+
+    /// The base interval descriptor.
+    public let interval: OrderedIntervalDescriptor
+
+    /// The amount of octaves displaced.
+    public let octaveDisplacement: Int
+}
+
+extension CompoundIntervalDescriptor {
+
+    // MARK: - Initializers
+
+    /// Creates a `CompoundIntervalDescriptor` with the given `interval` and the amount of `octaves`
+    /// of displacement.
+    public init(_ interval: OrderedIntervalDescriptor, displacedBy octaves: Int = 0) {
+        self.interval = interval
+        self.octaveDisplacement = octaves
+    }
+
+    /// Creates a `CompoundIntervalDescriptor` with the given `quality` and the given `ordinal`,
+    /// with no octave displacement.
+    public init(_ quality: IntervalQuality, _ ordinal: OrderedIntervalDescriptor.Ordinal) {
+        self.init(OrderedIntervalDescriptor(quality,ordinal))
+    }
+}
+
+extension CompoundIntervalDescriptor: Equatable { }
+extension CompoundIntervalDescriptor: Hashable { }
