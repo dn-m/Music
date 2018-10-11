77863c480918ae266591241efd81293187961f14
James <jbean46@gmail.com>
2018-10-10 21:12:45 -0400

    Migrate IntervalQualityTests

---
+++ Tests/PitchTests/IntervalQualityTests.swift
@@ -0,0 +1,31 @@
+//
+//  IntervalQualityTests.swift
+//  PitchTests
+//
+//  Created by James Bean on 10/10/18.
+//
+
+import XCTest
+import Pitch
+
+class IntervalQualityTests: XCTestCase {
+
+    func testInverseDimAug() {
+        let dim = IntervalQuality.extended(.init(.single, .diminished))
+        let aug = IntervalQuality.extended(.init(.single, .augmented))
+        XCTAssertEqual(dim.inverse, aug)
+        XCTAssertEqual(aug.inverse, dim)
+    }
+
+    func testInverseMinorMajor() {
+        let maj = IntervalQuality.imperfect(.major)
+        let min = IntervalQuality.imperfect(.minor)
+        XCTAssertEqual(maj.inverse, min)
+        XCTAssertEqual(min.inverse, maj)
+    }
+
+    func testInversePerfect() {
+        let perfect = IntervalQuality.perfect(.perfect)
+        XCTAssertEqual(perfect.inverse, perfect)
+    }
+}
