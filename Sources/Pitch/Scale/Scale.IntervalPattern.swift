174d803989659fb9a94f9692a74b0f919d0a70c9
James <jbean46@gmail.com>
2018-10-23 09:28:37 -0400

    Update Scale docs

--- Sources/Pitch/Scale/Scale.IntervalPattern.swift
+++ Sources/Pitch/Scale/Scale.IntervalPattern.swift
@@ -9,14 +9,35 @@ import DataStructures
 
 extension Scale {
 
+    // MARK: - Nested Types
+
+    /// The pattern of intervals which defines the quality of a `Scale`.
+    ///
+    /// **Example Usage**
+    ///
+    ///     let major: IntervalPattern = [2,2,1,2,2,2]
+    ///     let wholeTone: IntervalPattern = [2,2,2,2,2,2]
+    ///     let octatonic12: IntervalPattern = .octatonic12
+    ///     let tetrachord = IntervalPattern([2,2,1], isLooping: false)
+    ///
     public struct IntervalPattern {
+
+        // MARK: - Instance Properties
+
+        /// The pitch intervals comprising this `IntervalPattern`.
         let intervals: [Pitch]
+
+        /// Whether this `IntervalPattern` protracts infinitely upward.
         let isLooping: Bool
     }
 }
 
 extension Scale.IntervalPattern {
 
+    // MARK: - Initializers
+
+    /// Creates a `Scale.IntervalPattern` with the given `intervals` and whether or not to
+    /// protract infinitely upward.
     public init(_ intervals: [Pitch], isLooping: Bool = true) {
         self.init(intervals: intervals, isLooping: isLooping)
     }
@@ -41,14 +62,35 @@ extension Scale.IntervalPattern {
 }
 
 extension Scale.IntervalPattern {
-    static var chromatic: Scale.IntervalPattern { return [1,1,1,1,1,1,1,1,1,1,1,1] }
-    static var major: Scale.IntervalPattern { return [2,2,1,2,2,2,1] }
-    static var minor: Scale.IntervalPattern { return [2,1,2,2,1,2,2] }
-    static var melodicMinorAscending: Scale.IntervalPattern { return [2,1,2,2,2,2,1] }
-    static var melodicMinorDescending: Scale.IntervalPattern { return .minor }
-    static var harmonicMinor: Scale.IntervalPattern { return [2,1,2,2,1,3,1] }
-    static var octatonic21: Scale.IntervalPattern { return [2,1,2,1,2,1,2,1] }
-    static var octatonic12: Scale.IntervalPattern { return [1,2,1,2,1,2,1,2] }
+
+    // MARK: - Type Properties
+
+    /// Chromatic scale interval pattern.
+    public static let chromatic: Scale.IntervalPattern = [1,1,1,1,1,1,1,1,1,1,1,1]
+
+    /// Major scale interval pattern.
+    public static let major: Scale.IntervalPattern = [2,2,1,2,2,2,1]
+
+    /// Chromatic scale interval pattern.
+    public static let minor: Scale.IntervalPattern = [2,1,2,2,1,2,2]
+
+    /// Melodic minor ascending scale interval pattern.
+    public static let melodicMinorAscending: Scale.IntervalPattern = [2,1,2,2,2,2,1]
+
+    /// Melodic minor descending scale interval pattern.
+    public static let melodicMinorDescending: Scale.IntervalPattern = .minor
+
+    /// Harmonic minor scale interval pattern.
+    public static let harmonicMinor: Scale.IntervalPattern = [2,1,2,2,1,3,1]
+
+    /// Octatonic 2-1 scale interval pattern.
+    public static let octatonic21: Scale.IntervalPattern = [2,1,2,1,2,1,2,1]
+
+    /// Octatonic 1-2 scale interval pattern.
+    public static let octatonic12: Scale.IntervalPattern = [1,2,1,2,1,2,1,2]
+
+    /// Whole tone scale interval pattern.
+    public static let wholeTone: Scale.IntervalPattern = [2,2,2,2,2,2]
 }
 
 extension Scale.IntervalPattern: Sequence {
