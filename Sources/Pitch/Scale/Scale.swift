0f6410b9898a96bba09fa8ef3d50b0a5a6952adf
James Bean <jbean46@gmail.com>
2018-10-23 10:29:35 -0400

    Update Scale docs (#110)

--- Sources/Pitch/Scale/Scale.swift
+++ Sources/Pitch/Scale/Scale.swift
@@ -8,17 +8,34 @@
 import Algebra
 import DataStructures
 
+/// Ordered sequence of pitch intervals.
+///
+/// A `Scale` may or may not be octave invariant, and it may or may not be infinitely protracting.
+///
+/// **Example Usage**
+///
+///     let cMajor: Scale = [0,2,4,5,7,9,11]
+///     let cSharpWholeTone = Scale(1, [2,2,2,2,2,2])
+///     let gHarmonicMinor = Scale(7, .harmonicMinor)
+///
 public struct Scale {
 
     // MARK: - Instance Properties
 
-    let root: Pitch
-    let intervals: IntervalPattern
+    /// The root (lowest) pitch of this `Scale.`
+    public let root: Pitch
+
+    /// The intervals which comprise this `Scale`.
+    public let intervals: IntervalPattern
 }
 
 extension Scale {
-    var isOctaveRepeating: Bool {
-        return intervals.span == 12 && intervals.isLooping
+
+    // MARK: - Computed Properties
+
+    /// - Returns: `true` if the intervals contained herein span 12 semitones. Otherwise, `false`.
+    public var isOctaveInvariant: Bool {
+        return intervals.span == 12
     }
 }
 
