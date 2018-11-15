f0b7f3b863b0e927cb4165c994256e84bad7fec8
James <jbean46@gmail.com>
2018-11-15 12:16:09 -0500

    Streamline Pitch.Class.inversion implementation

--- Sources/Pitch/Pitch.Class.swift
+++ Sources/Pitch/Pitch.Class.swift
@@ -29,7 +29,7 @@ extension Pitch {
         ///     let light = dark.inversion // => 8
         ///
         public var inversion: Pitch.Class {
-            return Pitch.Class(NoteNumber(12) - value)
+            return Pitch.Class(12 - value)
         }
 
         /// Value of `Pitch.Class`.
